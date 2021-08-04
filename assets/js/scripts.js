b4d2d94926ce24697a3fd5bbc16299c08505538c
Andres <andreseguanipa1@gmail.com>
2021-08-04 15:09:33 -0400

    Primer commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,18 @@
+let publicidadOne = document.getElementById("publicidad-1");
+const botonOne = document.getElementById("boton-1");
+
+botonOne.addEventListener('click', function(){
+
+    publicidadOne.style.display = "none";
+
+});
+
+
+let publicidadTwo = document.getElementById("publicidad-2");
+const botonTwo = document.getElementById("boton-2");
+
+botonTwo.addEventListener('click', function(){
+
+    publicidadTwo.style.display = "none";
+
+});
